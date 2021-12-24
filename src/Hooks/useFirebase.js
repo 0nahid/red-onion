410dd0a2715a7ff2f5427674016d45465b9a4450
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-25 02:42:16 +0600

    fixed reload

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,27 +1,43 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import firebaseAuthentication from "../Firebase/firebase.init";
 
 firebaseAuthentication();
 export default function useFirebase() {
     const [user, setUser] = useState({});
     const [isLoading, setIsLoading] = useState(true);
-
+    const navigator = useNavigate();
     const auth = getAuth();
     const googleProvider = new GoogleAuthProvider();
     const signInWithGoogle = () => {
         setIsLoading(true);
-        return signInWithPopup(auth, googleProvider)
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                setUser(result.user);
+                navigator(-1); // -1 is the default value for the history.goBack()
+            })
             .finally(() => setIsLoading(false));
     }
-    const logout = () => {
-        signOut(auth).then(result => { }).then(error => { }).finally(() => setIsLoading(false));
-    }
+    // observe user state change
     useEffect(() => {
-        auth.onAuthStateChanged(user => {
-            setUser(user);
+        const unsubscribed = onAuthStateChanged(auth, user => {
+            if (user) {
+                setUser(user);
+            }
+            else {
+                setUser({})
+            }
+            setIsLoading(false);
         });
-        setIsLoading(false);
+        return () => unsubscribed;
     }, [auth])
+
+    const logout = () => {
+        setIsLoading(true);
+        signOut(auth)
+            .then(() => { })
+            .finally(() => setIsLoading(false));
+    }
     return { signInWithGoogle, logout, user, setIsLoading, isLoading };
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,29 +1,32 @@
 import { FcGoogle } from "react-icons/fc";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import useAuth from "../Hooks/useAuth";
 
 export default function Login() {
-  const { signInWithGoogle, user, setIsLoading } = useAuth();
-  const navigator = useNavigate();
-  const handleGoogleSignIn = () => {
-    signInWithGoogle()
-      .then((result) => {
-        navigator(-1); // -1 is the default value for the history.goBack()
-      })
-      .then((error) => {})
-      .finally(() => setIsLoading(false));
-  };
+  const { signInWithGoogle, user } = useAuth();
+    // const navigator = useNavigate();
+    // const handleGoogleSignIn = () => {
+    //   signInWithGoogle()
+    //     .then((result) => {
+    //       navigator(-1); // -1 is the default value for the history.goBack()
+    //     })
+    //     .then((error) => {})
+    //     .finally(() => setIsLoading(false));
+    // };
   return (
     <div className="grid place-items-center h-screen">
       {user?.email ? (
         <span>
-          Welcome {user?.displayName} <br /> <Link className="underline" to="/">Go to Home</Link>{" "}
+          Welcome {user?.displayName} <br />{" "}
+          <Link className="underline" to="/">
+            Go to Home
+          </Link>{" "}
         </span>
       ) : (
         <div className="border-t mx-auto border-white bg-gray-50 mt-6">
           <div
             className="flex items-center space-x-3 justify-center border border-gray-300 rounded-lg w-full py-3 px-4 cursor-pointer hover:bg-gray-100"
-            onClick={handleGoogleSignIn}
+            onClick={signInWithGoogle}
           >
             <FcGoogle className="w-6 h-6" />
             <span>Sign in With Google</span>
