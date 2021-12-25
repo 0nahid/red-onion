5e2886f3d2cc02fbeb14e8fa8cda02c483de236f
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-26 00:05:44 +0600

    added about us page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
+import Banner from "../Components/Banner/Banner";
 import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header";
-import Banner from "../Components/Banner/Banner";
+import About from "./About";
 export default function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <About />
       <Footer />
     </>
   );
