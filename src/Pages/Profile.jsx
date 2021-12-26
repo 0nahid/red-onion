7b127c040b1d15845550d4ea1ad7b27a41f91a3e
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-26 12:50:55 +0600

    changed styles on profile page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -5,7 +5,7 @@ export default function Profile() {
   return (
     <>
       <Header />
-      <div className="grid place-items-center h-screen bg-gray-50">
+      <div className="grid place-items-center h-screen bg-gray-900">
         <figure className="md:flex bg-black text-white rounded-xl p-8 md:p-0  overflow-hidden ">
           <img
             className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
@@ -15,18 +15,20 @@ export default function Profile() {
             height="512"
           />
           <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-            <blockquote>
-              <p className="text-lg font-medium">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Dolore, dolorem!
-              </p>
-            </blockquote>
             <figcaption className="font-medium">
               <div className="text-sky-500 dark:text-sky-400">
                 {user?.displayName}
               </div>
               <div className="text-gray-700 dark:text-gray-500">
                 Staff Engineer, Algolia
+              </div>
+              <div className=" font-light ">
+                Email: <span>{user?.email}</span> <br />
+                UID: <span>{user?.uid}</span>
+              </div>
+              <div>
+                Last Sign in time :{" "}
+                <small>{user?.metadata?.lastSignInTime}</small>
               </div>
             </figcaption>
           </div>
