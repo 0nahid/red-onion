e761dd0647b277c40137a96f1a7bd4c153c0bafc
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-27 00:08:04 +0600

    added responsive nav

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,11 +6,8 @@ export default function Header() {
   return (
     <>
       <header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <Link
-            to="/"
-            className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
-          >
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row ">
+          <Link to="/" className="mb-4 md:mb-0">
             <Logo />
           </Link>
           <nav className="md:ml-auto  flex flex-wrap items-center text-base justify-center ">
@@ -45,12 +42,12 @@ export default function Header() {
           {user?.email ? (
             <button
               onClick={logout}
-              className="inline-flex items-center bg-[#f91944] text-white border-0 py-1.5 px-4 focus:outline-none rounded-full text-base mt-4 md:mt-0"
+              className="inline-flex justify-center bg-[#f91944] text-white border-0 py-1.5 px-4 focus:outline-none rounded-full text-base mt-4 md:mt-0"
             >
               Logout
             </button>
           ) : (
-            <button className="inline-flex items-center bg-[#f91944] text-white border-0 py-1.5 px-4 focus:outline-none rounded-full text-base mt-4 md:mt-0">
+            <button className="inline-flex bg-[#f91944] text-white border-0 py-1.5 px-4 focus:outline-none rounded-full text-base mt-4 md:mt-0">
               <Link to="/signup">Sign up</Link>
             </button>
           )}
