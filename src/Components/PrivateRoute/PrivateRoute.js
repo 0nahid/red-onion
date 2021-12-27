c62990f3d9128122502e177fec1142fb76e0db5a
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-27 17:33:09 +0600

    added loading spinner

--- src/Components/PrivateRoute/PrivateRoute.js
+++ src/Components/PrivateRoute/PrivateRoute.js
@@ -2,24 +2,12 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import useAuth from "../../Hooks/useAuth";
 
-export default function PrivateRoute({ children, ...rest }) {
+export default function PrivateRoute({ children }) {
     const { user, isLoading } = useAuth();
     if (isLoading) {
         return (
-            <div class="border border-blue-300 shadow rounded-md p-4 w-full mx-auto h-screen">
-                <div class="animate-pulse flex space-x-4">
-                    <div class="rounded-full bg-gray-200 h-10 w-10"></div>
-                    <div class="flex-1 space-y-6 py-1">
-                        <div class="h-2 bg-gray-200 rounded"></div>
-                        <div class="space-y-3">
-                            <div class="grid grid-cols-3 gap-4">
-                                <div class="h-2 bg-gray-200 rounded col-span-2"></div>
-                                <div class="h-2 bg-gray-200 rounded col-span-1"></div>
-                            </div>
-                            <div class="h-2 bg-gray-200 rounded"></div>
-                        </div>
-                    </div>
-                </div>
+            <div className="flex place-items-center h-screen justify-center items-center">
+                <div className="animate-spin rounded-full h-32 w-32 border-b-4 border-[#f91944]"></div>
             </div>
         );
     }
